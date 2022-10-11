6eb4b2bd90a5cad8af20c85aa9498b82d50e6f34
Richard Breno <filip_2001_richard2022@hotmail.com>
2022-10-11 17:05:25 -0300

    commit

---
+++ ex001/ex01.js
@@ -0,0 +1,14 @@
+var comida = 'Frango'
+switch (comida) {
+    case 'Frango':
+        console.log('Proteína')
+    break
+
+    case 'Arroz':
+        console.log('Carboídrato')
+    break
+
+    default:
+    console.log('ERRO!')
+    break
+}
