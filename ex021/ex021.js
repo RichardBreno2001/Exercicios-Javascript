dab71f88b663e492939abdb95c10b7b6a9c27de5
Richard Breno <filip_2001_richard2022@hotmail.com>
2022-12-19 17:20:30 -0300

    commit

---
+++ ex021/ex021.js
@@ -0,0 +1,16 @@
+let cachorro = {
+    nome: 'Scooby',
+    patas: 4,
+    cidade: 'Curitiba',
+    latir: function() {
+        console.log(`Au Au`)
+    }
+}
+
+console.log(cachorro.nome)
+
+console.log(cachorro.patas)
+
+cachorro.latir()  //Invocando a função que está dentro do objeto
+
+
