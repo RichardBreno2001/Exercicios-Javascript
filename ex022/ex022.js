38b4e7521a65523c5fb8fe149589a5f5b5712400
Richard Breno <filip_2001_richard2022@hotmail.com>
2022-12-22 18:10:55 -0300

    commit

---
+++ ex022/ex022.js
@@ -0,0 +1,18 @@
+let objeto1 = {
+    testeA:'Primeiro',
+    testeB:'Segundo',
+    testeC:'Terceiro',
+}
+
+let objeto2 = {
+    testeD: 'Quarto',
+}
+
+Object.assign(objeto1,objeto2)
+
+console.log(objeto1)
+
+console.log(Object.keys(objeto1))
+
+
+
