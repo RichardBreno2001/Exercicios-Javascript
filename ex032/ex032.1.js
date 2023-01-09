5e389efb686e3f91870343034dd57ee5f1ce3ede
Richard Breno <filip_2001_richard2022@hotmail.com>
2023-01-08 23:18:23 -0300

    commit

---
+++ ex032/ex032.1.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    "Nome": "Breno",
+    "Idade":21,
+    "Linguagens":["Javascript","CSS","HTML"]
+}
+
+console.log(pessoa.Nome)
+console.log(pessoa.Linguagens)
+
+console.log(pessoa.Linguagens[0])
+
