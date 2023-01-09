f4ca1ed6a49409544538ce877152290308b0f50c
Richard Breno <filip_2001_richard2022@hotmail.com>
2023-01-08 23:42:28 -0300

    commit

---
+++ ex032/ex032.2.js
@@ -0,0 +1,12 @@
+let testeJSON = {
+    "nome":"Breno",
+    "idade":21,
+    "linguagens":["Javascript","HTML","CSS"]
+}
+
+let converterParaString = JSON.stringify(testeJSON)
+
+console.log(converterParaString)
+
+//Convertendo JSON para string
+
